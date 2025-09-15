cf5a56d580bed0801225d5753a06f07b2774f8c0
KRISH PATEL <krishpatel2136@gmail.com>
2025-09-15 17:26:55 +0530

    Update DBManager.java

--- src/dao/DBManager.java
+++ src/dao/DBManager.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBManager {
     private static final String URL = "jdbc:mysql://localhost:3306/revForce_motosports";
-    private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String USER = "YOUR_USERNAME";
+    private static final String PASS = "YOUR_PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
